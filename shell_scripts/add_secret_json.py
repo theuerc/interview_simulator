c04bc0e58af6eee00f9341d5b5717b21832e7bb5
Coulton Theuer <theuerc@umich.edu>
2023-08-05 23:32:56 +0000

    fix linting

--- shell_scripts/add_secret_json.py
+++ shell_scripts/add_secret_json.py
@@ -1,9 +1,10 @@
-"""Write google secret json for gitpod initialization"""
+"""Write google secret json for gitpod initialization."""
 import argparse
 import json
 
+
 def write_to_json(filename, dic_str):
-    """write string to json"""
+    """Write string to json."""
     # Convert the string to a dictionary
     dic = json.loads(dic_str)
 
@@ -11,11 +12,14 @@ def write_to_json(filename, dic_str):
     with open(filename, "w") as json_file:
         json.dump(dic, json_file, indent=4)
 
+
 if __name__ == "__main__":
     """take in arguments and convert string to json"""
     parser = argparse.ArgumentParser(description="Write a JSON string to a file.")
     parser.add_argument("filename", type=str, help="The name of the file to write to.")
-    parser.add_argument("dic_string", type=str, help="The JSON string to write to the file.")
+    parser.add_argument(
+        "dic_string", type=str, help="The JSON string to write to the file."
+    )
 
     args = parser.parse_args()
 
