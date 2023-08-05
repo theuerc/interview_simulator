66384db4747ed3a40ceb6066bb7c6a9297cb3e76
Coulton Theuer <theuerc@umich.edu>
2023-08-05 20:30:43 +0000

    add gitpod secret integration

---
+++ shell_scripts/add_secret_json.py
@@ -0,0 +1,22 @@
+"""Write google secret json for gitpod initialization"""
+import json
+import argparse
+
+def write_to_json(filename, dic_str):
+    """write string to json"""
+    # Convert the string to a dictionary
+    dic = json.loads(dic_str)
+
+    # Write the dictionary to a JSON file
+    with open(filename, "w") as json_file:
+        json.dump(dic, json_file, indent=4)
+
+if __name__ == "__main__":
+    """take in arguments and convert string to json"""
+    parser = argparse.ArgumentParser(description="Write a JSON string to a file.")
+    parser.add_argument("filename", type=str, help="The name of the file to write to.")
+    parser.add_argument("dic_string", type=str, help="The JSON string to write to the file.")
+
+    args = parser.parse_args()
+
+    write_to_json(args.filename, args.dic_string)
